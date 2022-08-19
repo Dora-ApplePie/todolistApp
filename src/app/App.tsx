6274db823a78bc78ee72344d712c49575f8e2c0c
Dora-ApplePie <dasha0000golenko@gmail.com>
2022-08-20 00:50:33 +0300

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,70 @@
+import React, {useEffect} from 'react'
+import './App.css'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import LinearProgress from '@mui/material/LinearProgress';
+import {Menu} from '@mui/icons-material';
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {useAppDispatch, useAppSelector} from "./hooks";
+import {Login} from "../features/Login/Login";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {initializeAppTC} from "./app-reducer";
+import CircularProgress from "@mui/material/CircularProgress";
+import {logoutTC} from "../features/Login/auth-reducer";
+
+type PropsType = {
+    demo?: boolean
+}
+
+function App({demo = false}: PropsType) {
+
+    const dispatch = useAppDispatch();
+    const status = useAppSelector((state) => state.app.status)
+    const isInitialized = useAppSelector((state) => state.app.isInitialized)
+    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
+
+    useEffect(() => {
+        dispatch(initializeAppTC())
+    })
+
+    if(!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
+
+    const logoutHandler = () => {
+        dispatch(logoutTC())
+    }
+
+    return (
+        <div className="App">
+            <ErrorSnackbar/>
+            <AppBar position="static">
+                <Toolbar style={{display: "flex", justifyContent: "space-between"}}>
+                    <Typography variant="h6">
+                        Todolist
+                    </Typography>
+                    {isLoggedIn && <Button variant="outlined" color="inherit" onClick={logoutHandler}>Log out</Button>}
+                </Toolbar>
+                {status === 'loading' && <LinearProgress/>}
+            </AppBar>
+            <Container fixed>
+                <Routes>
+                    <Route path="/" element={<TodolistsList demo={demo}/>}/>
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="404" element={<h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>}/>
+                    <Route path="*" element={<Navigate to={'404'}/>}/>
+                </Routes>
+            </Container>
+        </div>
+    )
+}
+
+export default App
---
+++ src/features/Login/auth-reducer.ts
@@ -0,0 +1,62 @@
+import { Dispatch } from 'redux'
+import { SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType } from '../../app/app-reducer'
+import {authAPI, LoginParamsType} from "../../api/todolists-api";
+import {handleServerAppError, handleServerNetworkError} from "../../utils/error-utils";
+import {AppThunk} from "../../app/store";
+
+const initialState = {
+    isLoggedIn: false
+}
+type InitialStateType = typeof initialState
+
+export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'login/SET-IS-LOGGED-IN':
+            return {...state, isLoggedIn: action.isLoggedIn}
+        default:
+            return state
+    }
+}
+
+// actions
+export const setIsLoggedInAC = (isLoggedIn: boolean) =>
+    ({type: 'login/SET-IS-LOGGED-IN', isLoggedIn} as const)
+
+// thunks
+export const loginTC = (data: LoginParamsType):AppThunk => (dispatch: Dispatch<ActionsType>)  => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.login(data)
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                const action = setIsLoggedInAC(true)
+                dispatch(action)
+                dispatch(setAppStatusAC('succeeded'))
+            } else {
+                handleServerAppError(res.data, dispatch);
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
+
+export const logoutTC = ():AppThunk => (dispatch: Dispatch<ActionsType>) => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.logout()
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(false))
+                dispatch(setAppStatusAC('succeeded'))
+            } else {
+                handleServerAppError(res.data, dispatch)
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
+
+
+// types
+type ActionsType = ReturnType<typeof setIsLoggedInAC> | SetAppStatusActionType | SetAppErrorActionType
+
